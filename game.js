63d6c96004b6de119e7690b26338831abf0c6b7b
Mehrshad <bmehrshad@myseneca.ca>
2025-07-23 01:11:28 -0400

    make sound animate press was added

--- game.js
+++ game.js
@@ -3,25 +3,55 @@ var userClickedPattern = [];
 
 var gamePattern = [];
 
-var blueAudio = new Audio("./sounds/blue.mp3");
-var greenAudio = new Audio("./sounds/green.mp3");
-var redAudio = new Audio("./sounds/red.mp3");
-var yellowAudio = new Audio("./sounds/yellow.mp3");
-var wrongAudio = new Audio("./sounds/wrong.mp3");
+var blueAudio = new Audio("sounds/blue.mp3");
+var greenAudio = new Audio("sounds/green.mp3");
+var redAudio = new Audio("sounds/red.mp3");
+var yellowAudio = new Audio("sounds/yellow.mp3");
+var wrongAudio = new Audio("sounds/wrong.mp3");
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  var randomChosenColour = buttonColours[randomNumber];
+
+  buttonColours.push(randomChosenColour);
+
+  $(`#${randomChosenColour}`).fadeOut("10").fadeIn("10");
+  makeSound(randomChosenColour);
 }
-
-var randomChosenColour = buttonColours[nextSequence()];
-
-buttonColours.push(randomChosenColour);
-
-$(`#${randomChosenColour}`).fadeOut("10").fadeIn("10");
 
 $(".btn").on("click", function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
+  makeSound(userChosenColour);
+  animatePress(userChosenColour);
 });
+
+function makeSound(key) {
+  switch (key) {
+    case "green":
+      greenAudio.play();
+      break;
+    case "blue":
+      blueAudio.play();
+      break;
+
+    case "red":
+      redAudio.play();
+      break;
+
+    case "yellow":
+      yellowAudio.play();
+      break;
+
+    default:
+      wrongAudio.play();
+      break;
+  }
+}
+
+function animatePress(currentcolour) {
+  $(`#${currentcolour}`).addClass("pressed");
+  setTimeout(function () {
+    $(`#${currentcolour}`).removeClass("pressed");
+  }, 100);
+}
